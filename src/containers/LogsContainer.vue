6bd0424ff0fb5ae00d8015c1d8be0419e5f2bd74
Art <Artawower@mail.ru>
2025-12-02 02:19:23 +0400

    feat: logs container and command

---
+++ src/containers/LogsContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <safe-area fit>
+    <container-layout gap="lg" :body-scroll="false">
+      <app-logs />
+
+      <template #footer>
+        <card-wrapper>
+          <menu-item type="info" @click="safeCopyToClipboard(errorLogText)">
+            {{ $t(I18N.COPY_LOG) }}
+          </menu-item>
+        </card-wrapper>
+      </template>
+    </container-layout>
+  </safe-area>
+</template>
+
+<script setup lang="ts">
+import ContainerLayout from 'src/components/ContainerLayout.vue';
+import AppLogs from 'src/containers/AppLogs.vue';
+import { I18N } from 'orgnote-api';
+import { useAppLogs } from 'src/composables/useAppLogs';
+import MenuItem from 'src/containers/MenuItem.vue';
+import CardWrapper from 'src/components/CardWrapper.vue';
+import SafeArea from 'src/components/SafeArea.vue';
+import { useInteractiveClipboard } from 'src/composables/use-interactive-clipboard';
+
+const { errorLogText } = useAppLogs();
+const { safeCopyToClipboard } = useInteractiveClipboard();
+</script>
